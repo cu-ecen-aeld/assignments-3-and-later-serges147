4e8953947437544b58a5424802fb85dad01102a7
Sergei Shirokov <sergej.shirokov@gmail.com>
2024-12-15 11:27:01 +0200

    added finder

---
+++ finder-app/finder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "2 arguments expected"
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ -d ${filesdir} ]
+then
+    x=$( find ${filesdir} -type f | wc -l )
+    y=$( grep -r "${searchstr}" "${filesdir}" | wc -l )
+    echo "The number of files are ${x} and the number of matching lines are ${y}"
+else
+    echo "not a directory"
+    exit 1
+fi
